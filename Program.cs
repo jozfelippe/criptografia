60e93dd9b7d9fc39b2cc175e549d147548b52a3b
José Felipe e Ana Beatriz <josefelipesilvasantos874@gmail.com>
2023-04-23 17:39:01 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.ResetColor();
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("DIGITE UMA FRASE PARA ENCRIPTAR:");
+string frase = Console.ReadLine()!;
+
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("DIGITE UMA CHAVE (NÚMERO):");
+int numero = Convert.ToInt32(Console.ReadLine())!;
+
+    string fraseNormal = frase.ToLower(); 
+    string fraseEncriptada = fraseNova(fraseNormal, numero);
+
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("FRASE ESCOLHIDA: " + frase);
+Console.WriteLine("FRASE ENCRIPTADA: " + fraseEncriptada);
+Console.ResetColor();
+Console.WriteLine("");
+
+    static string fraseNova(string frase, int numero)
+        {
+        char[] letras = frase.ToCharArray();
+        string novaFrase = "";
+
+            foreach (char letra in letras)
+            {
+            if (char.IsLetter(letra)) 
+            {
+            char novaLetra = (char)(((letra - 'a' + numero) % 26) + 'a'); 
+
+            novaFrase += novaLetra;
+            }
+            else
+            {
+            novaFrase += letra; 
+            }
+            }
+
+        return novaFrase;
+        }
